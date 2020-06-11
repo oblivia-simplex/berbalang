59d8756f50082264b5edcba51b14ea3ab428db27
lucca <lucca.fraser@gmail.com>
2020-06-11 16:38:00 -0300

    nicer naming scheme

--- src/util/name.rs
+++ src/util/name.rs
@@ -1,6 +1,7 @@
+use crate::util::five_letter_words::WORDS;
 use rand::Rng;
 
-pub fn random(syllables: usize) -> String {
+pub fn random_syllables(syllables: usize) -> String {
     let mut rng = rand::thread_rng();
     let consonants = [
         'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w',
@@ -19,4 +20,21 @@ pub fn random(syllables: usize) -> String {
     }
 
     s.iter().collect()
+}
+
+pub fn random_words(words: usize) -> String {
+    let mut rng = rand::thread_rng();
+    let mut s = String::new();
+    let n = WORDS.len();
+    for i in 0..words {
+        s.push_str(WORDS[rng.gen_range(0, n)]);
+        if i + 1 < words {
+            s.push_str("-")
+        }
+    }
+    s
+}
+
+pub fn random(parts: usize) -> String {
+    random_words(parts)
 }
