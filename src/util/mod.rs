a4a16e7f3032c005b96cabae88d8914ba53853bd
lucca <lucca.fraser@gmail.com>
2020-06-14 15:55:49 -0300

    defined hash_seed_rng

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -4,3 +4,4 @@ pub mod count_min_sketch;
 pub mod distance;
 pub mod five_letter_words;
 pub mod name;
+pub mod random;
---
+++ src/util/random.rs
@@ -0,0 +1,22 @@
+use rand::prelude::StdRng;
+use rand::{Rng, SeedableRng};
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+pub fn hash_seed_rng<H: Hash>(thing: &H) -> impl Rng {
+    let seed = hash_seed(thing);
+    // put the epoch back together
+    let rng = StdRng::from_seed(seed);
+    rng
+}
+
+pub fn hash_seed<H: Hash>(thing: &H) -> [u8; 32] {
+    let mut h = DefaultHasher::new();
+    thing.hash(&mut h);
+    let hash = h.finish();
+    let mut seed = [0_u8; 32];
+    for i in 0..32 {
+        seed[i] = ((hash >> (i * 2) as u64) & 0xFF) as u8;
+    }
+    seed
+}
