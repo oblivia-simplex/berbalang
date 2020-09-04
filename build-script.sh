e68578bf26948d0b9baf9a9d920dffb540c587c1
Daniel Kuehn <daniel@kuehn.se>
2020-09-04 20:30:05 +0200

    Add building capstone

--- build-script.sh
+++ build-script.sh
@@ -8,7 +8,7 @@ build_dependency () {
   download_target="${3}"
   dir_name="${4}"
 
-  wget "${download_url}" -O "${download_target}" && "${unpack_cmd}" "${download_target}"
+  wget "${download_url}" -O "${download_target}" && ${unpack_cmd} "${download_target}"
   pushd "${dir_name}"
   make && make install
   popd
@@ -27,4 +27,7 @@ mkdir -p /tmp/work
 pushd /tmp/work
 
 echo "Building unicorn emulator library"
-build_dependency https://github.com/unicorn-engine/unicorn/archive/1.0.1.zip 'unzip' unicorn_src.zip unicorn-1.0.1
+build_dependency https://github.com/unicorn-engine/unicorn/archive/1.0.1.zip "unzip" unicorn_src.zip unicorn-1.0.1
+
+echo "Building capstone disassembly library"
+build_dependency https://github.com/aquynh/capstone/archive/4.0.2.tar.gz "tar -xvzf" capstone_src.tar.gz capstone-4.0.2
