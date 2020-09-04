30a1953f8a0cd3203efb3afa7cd8c4810ca9e6b9
Daniel Kuehn <daniel@kuehn.se>
2020-09-04 20:09:31 +0200

    Add set -e so that any errored command will exit the script and add a function to easily build dependencies

--- build-script.sh
+++ build-script.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
 
-echo "Hello, world!"
-echo "From inside the container!"
+set -e
+
+build_dependency () {
+  download_url="${1}"
+  unpack_cmd="${2}"
+  download_target="${3}"
+  dir_name="${4}"
+
+  wget "${download_url}" -O "${download_target}" && "${unpack_cmd}" "${download_target}"
+  pushd "${dir_name}"
+  make && make install
+  popd
+  rm -rf "${dir_name}"
+}
+
+echo "Sleeping for 5s to let networking settle"
+sleep 5
+
+echo "Updating the cache"
+apt update -y
+echo "Installing tooling"
+apt install -y build-essential clang llvm-dev python unzip wget
+
+mkdir -p /tmp/work
+pushd /tmp/work
+
+echo "Building unicorn emulator library"
+build_dependency https://github.com/unicorn-engine/unicorn/archive/1.0.1.zip 'unzip' unicorn_src.zip unicorn-1.0.1
