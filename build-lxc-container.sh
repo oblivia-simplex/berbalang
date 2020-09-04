bf40f7f12302cbe85ea0d3dd6732703a3dff69c9
Daniel Kuehn <daniel@kuehn.se>
2020-09-04 19:51:11 +0200

    Initial script for automating building LXC containers for berbalang

---
+++ build-lxc-container.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Adapted from the Dockerfile
+
+##################################
+# First, build the builder image #
+##################################
+builder_exists=$(lxc list | grep berbalang-builder)
+
+if [ "${builder_exists}" != "" ]; then
+  echo "Found a old builder instance, removing..."
+  lxc stop berbalang-builder
+  lxc delete berbalang-builder
+fi
+
+lxc launch images:debian/buster berbalang-builder
+lxc file push ./build-script.sh berbalang-builder/root/
+lxc exec berbalang-builder -- '/root/build-script.sh'
