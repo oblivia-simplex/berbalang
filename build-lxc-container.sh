6c555213ea99706dd15d562e91bb32c6bc66cd64
Daniel Kuehn <daniel@kuehn.se>
2020-09-04 22:06:37 +0200

    Add a check for argument and the initial setting up of the real container

--- build-lxc-container.sh
+++ build-lxc-container.sh
@@ -3,6 +3,15 @@
 
 set -e
 
+if [ $# -ne 1 ]; then
+  echo "You need to supply a container name!"
+  echo "Usage:"
+  echo "$0 <container name>"
+  exit 1
+else
+  container_name="${1}"
+fi
+
 ##################################
 # First, build the builder image #
 ##################################
@@ -17,3 +26,31 @@ fi
 lxc launch images:debian/buster berbalang-builder
 lxc file push ./build-script.sh berbalang-builder/root/
 lxc exec berbalang-builder -- '/root/build-script.sh'
+
+mkdir -p tmp
+pushd tmp
+echo "Pulling build artefacts from berbalang-builder"
+lxc file pull berbalang-builder/berbalang.tar.xz .
+
+container_exists=$(lxc config show "${container_name}")
+
+if [ "${container_exists}" != "" ]; then
+  echo "${container_name} already exists!"
+  echo "Want to remove it? [Y/n]"
+  read CHOICE
+
+  if [ "${CHOICE}" == "y" || "${CHOICE}" == "Y" || "${CHOICE}" == "" ]; then
+    echo "Removing ${container_name}"
+    lxc stop "${container_name}"
+    lxc delete "${container_name}"
+  else
+    echo "You'll have to remove it manually or choose another name"
+    exit 1
+  fi
+fi
+
+echo "Creating container ${container_name}"
+lxc launch images:debian/buster "${container_name}"
+
+lxc file push berbalang.tar.xz "${container_name}"
+lxc exec "${container_name}" -- "tar xvf /berbalang.tar.xz -C /"
