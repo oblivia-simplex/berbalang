1fe48661d3ad1b9f7d817b757a4d18a0f2a57cb9
Daniel Kuehn <daniel@kuehn.se>
2020-09-04 23:48:42 +0200

    Add start/end banner and optional berbalang repo path argument

--- build-lxc-container.sh
+++ build-lxc-container.sh
@@ -9,17 +9,22 @@ err () {
 trap err ERR
 
 if [ $# -ne 1 ]; then
-  echo "You need to supply a container name!"
+  echo "You need to supply a container name! Optionally you can supply the path to the berbalang repo."
+  echo "If you don't supply a path to the berbalang repo, PWD is going to be assumed."
   echo "Usage:"
-  echo "$0 <container name>"
+  echo "$0 <container name> [/path/to/berbalang/repo]"
   exit 1
 else
+  PWD=$(pwd)
   container_name="${1}"
+  berbalang_repo_path="${2:=${PWD}}"
 fi
 
 ##################################
 # First, build the builder image #
 ##################################
+echo "Starting berbalang container creation"
+echo "Container name: ${container_name}\nBerbalang repo path: ${berbalang_repo_path}"
 echo "If you see Error: not found after this line, it just means it didn't find the berbalang-builder container already in the cluster"
 builder_exists=$(lxc config show berbalang-builder || true)
 
@@ -59,8 +64,20 @@ fi
 echo "Creating container ${container_name}"
 lxc launch images:debian/buster "${container_name}"
 
-# Sleep 5 to wait for the container to settle before trying to push files to it
+echo "Sleep 5s to wait for the container to settle before trying to push files to it"
 sleep 5
 
+echo "Pushing files to ${container_name}"
 lxc file push berbalang.tar.gz "${container_name}/"
 lxc exec "${container_name}" -- tar xvf /berbalang.tar.gz -C /
+lxc file push "${berbalang_repo_path}/start.sh" "${container_name}/root/"
+lxc file push "${berbalang_repo_path}/trials.sh" "${container_name}/root/"
+lxc file push --recursive "${berbalang_repo_path}/analysis" "${container_name}/root/analysis"
+printf "***********************************************************************************"
+printf "* container name: %-50s                *" "${container_name}"
+printf "* Your berbalang container is now ready!                                          *"
+printf "* Add the appropriate profile or devices                                          *"
+printf "* and run trials.sh with either                                                   *"
+printf "* `lxc exec <container name> -- trials.sh /root/experiments <trials> /root/logs`  *"
+printf "* or run `lxc shell <container name>` to enter the container and run it manually  *"
+printf "***********************************************************************************"
