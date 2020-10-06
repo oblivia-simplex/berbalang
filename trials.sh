f9398f276e226ad6c8395567089492d2c89ebdca
lucca <lucca.fraser@gmail.com>
2020-10-06 11:13:28 -0300

    backtrace

--- trials.sh
+++ trials.sh
@@ -6,5 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd)"
 
 cd $DIR
 
+export RUST_BACKTRACE=full
+
 #nix-shell -p python39Packages.pytz python39Packages.toml --run "./analysis/experiment.py $*"
 ./analysis/experiment.py $@
